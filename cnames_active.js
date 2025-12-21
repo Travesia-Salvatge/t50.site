d1f4ba5102c35f1ed4a311010ed6dc3f8330cb35
Abdullah Bin Shahid <theabdullahbinshahid@gmail.com>
2025-12-21 12:54:49 +0500

    Create cnames_active.js

---
+++ cnames_active.js
@@ -0,0 +1,49 @@
+/*
+ ***** ACTIVE SUBDOMAINS
+ * ***********************
+ *
+ * for the moment the following subdomains are active
+ * file a pull request to add your own GitHub Page
+ *
+ *
+ **** README
+ * ***********
+ *
+ * KEY:         The subdomain you request (e.g.: "foo" would give the custom domain "foo.t50.site").
+ *
+ * VALUE:       The regular domain provided by GitHub (e.g.: ".github.io" for a User-/Organization Page
+ *              or "/ /" for a Project Page).
+ *
+ * CLOUDFLARE:  T50.SITE uses Cloudflare as its DNS. By default, Cloudflare proxies all requests to your subdomain
+ *              to get SSL support (https://foo.t50.site) and make use of browser caching with a TTL of 30 min.
+ *              But you can opt-out from this and make Cloudflare forward all requests directly to GitHub.
+ *              Just add '// noCF' in the line of your requested subdomain to give us a hint.
+ *              (all the lines marked with '// noCF?' are from a time when a requester had to explicitly opt-in;
+ *              see: https://github.com/iemabdullah/t50.site//issues/)
+ *
+ * IMPORTANT:   To authorize yourself as the owner of the GitHub Page you must have added a valid CNAME file
+ *              (with the requested domain, e.g.: "foo.T50.SITE") to your repository.
+ *              The requested subdomain must match either your GitHub username or the name of your project
+ *              repository. Exceptions are possible for the sake of clarity (e.g. if your project has the
+ *              name "foojs" you can request "foo").
+ *              Before you start a request for your personal fork of "react" or "vue" you should read
+ *              the section "naming conflicts" in the wiki!
+ *
+ * NOTICE:      Please follow the format set by everyone else in this file. Entries should use double quotes,
+ *              with space between the colon (after the key) and the value and should always be in alphabetical
+ *              order. Each line apart from the very last entry should have a comma after it to ensure this file
+ *              is valid syntax.
+ *
+ *              BECAUSE OF THE RISK OF ABUSE, I'M NOT ACCEPTING REQUESTS FOR PAGES WITHOUT REASONABLE CONTENT!
+ */
+
+var cnames_active = {
+  "Tyoe_Doamin_Name": "t50.site.github.io",
+  "Shop": "shopno1.netlify.app"
+  
+  /*
+   * please don't add your subdomain records down here!
+   * insert them in alphabetical order to help reduce merge conflicts.
+   * <3
+   */
+}
